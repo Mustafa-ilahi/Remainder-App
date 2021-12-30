361ffae5645d4b5b6f810522ab317e75de4723dd
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-12-30 16:38:42 +0500

    getting started

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+export default function App() {
+  return (
+    <View>
+      <Text>Task monitoring App</Text>
+    </View>
+  )
+}
